52e1df50b196ea21a5f1c4ce28a2f9a4bba92170
Furkan EKİNCİ <furkanekinci@hotmail.com>
2019-01-15 01:04:04 +0300

    First upload.
    Creating repository.

---
+++ YouWatch/frmMain.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace YouWatch
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.chkMoveByMouse = new System.Windows.Forms.CheckBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMaximize = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.chkShowBorder = new System.Windows.Forms.CheckBox();
+            this.chkShowInTaskbar = new System.Windows.Forms.CheckBox();
+            this.chkKeepRatio = new System.Windows.Forms.CheckBox();
+            this.chkKeepTopOn = new System.Windows.Forms.CheckBox();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.timHider = new System.Windows.Forms.Timer(this.components);
+            this.wbbYouTube = new System.Windows.Forms.WebBrowser();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtURL.Location = new System.Drawing.Point(51, 6);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(327, 29);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtURL_KeyDown);
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.chkMoveByMouse);
+            this.pnlTop.Controls.Add(this.btnClose);
+            this.pnlTop.Controls.Add(this.btnMaximize);
+            this.pnlTop.Controls.Add(this.btnMinimize);
+            this.pnlTop.Controls.Add(this.chkShowBorder);
+            this.pnlTop.Controls.Add(this.chkShowInTaskbar);
+            this.pnlTop.Controls.Add(this.chkKeepRatio);
+            this.pnlTop.Controls.Add(this.chkKeepTopOn);
+            this.pnlTop.Controls.Add(this.btnGO);
+            this.pnlTop.Controls.Add(this.lblURL);
+            this.pnlTop.Controls.Add(this.txtURL);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(495, 60);
+            this.pnlTop.TabIndex = 2;
+            // 
+            // chkMoveByMouse
+            // 
+            this.chkMoveByMouse.AutoSize = true;
+            this.chkMoveByMouse.Enabled = false;
+            this.chkMoveByMouse.Location = new System.Drawing.Point(381, 40);
+            this.chkMoveByMouse.Name = "chkMoveByMouse";
+            this.chkMoveByMouse.Size = new System.Drawing.Size(103, 17);
+            this.chkMoveByMouse.TabIndex = 6;
+            this.chkMoveByMouse.Text = "Move By Mouse";
+            this.chkMoveByMouse.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(472, 7);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 28);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMaximize
+            // 
+            this.btnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximize.FlatAppearance.BorderSize = 0;
+            this.btnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximize.Location = new System.Drawing.Point(453, 7);
+            this.btnMaximize.Name = "btnMaximize";
+            this.btnMaximize.Size = new System.Drawing.Size(20, 28);
+            this.btnMaximize.TabIndex = 5;
+            this.btnMaximize.TabStop = false;
+            this.btnMaximize.Text = "F";
+            this.btnMaximize.UseVisualStyleBackColor = true;
+            this.btnMaximize.Click += new System.EventHandler(this.btnMaximize_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Location = new System.Drawing.Point(434, 7);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(20, 28);
+            this.btnMinimize.TabIndex = 5;
+            this.btnMinimize.TabStop = false;
+            this.btnMinimize.Text = "_";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // chkShowBorder
+            // 
+            this.chkShowBorder.AutoSize = true;
+            this.chkShowBorder.Checked = true;
+            this.chkShowBorder.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShowBorder.Location = new System.Drawing.Point(12, 41);
+            this.chkShowBorder.Name = "chkShowBorder";
+            this.chkShowBorder.Size = new System.Drawing.Size(87, 17);
+            this.chkShowBorder.TabIndex = 4;
+            this.chkShowBorder.Text = "Show Border";
+            this.chkShowBorder.UseVisualStyleBackColor = true;
+            this.chkShowBorder.CheckedChanged += new System.EventHandler(this.chkShowBorder_CheckedChanged);
+            // 
+            // chkShowInTaskbar
+            // 
+            this.chkShowInTaskbar.AutoSize = true;
+            this.chkShowInTaskbar.Location = new System.Drawing.Point(105, 41);
+            this.chkShowInTaskbar.Name = "chkShowInTaskbar";
+            this.chkShowInTaskbar.Size = new System.Drawing.Size(106, 17);
+            this.chkShowInTaskbar.TabIndex = 4;
+            this.chkShowInTaskbar.Text = "Show in Taskbar";
+            this.chkShowInTaskbar.UseVisualStyleBackColor = true;
+            this.chkShowInTaskbar.CheckedChanged += new System.EventHandler(this.chkShowInTaskbar_CheckedChanged);
+            // 
+            // chkKeepRatio
+            // 
+            this.chkKeepRatio.AutoSize = true;
+            this.chkKeepRatio.Location = new System.Drawing.Point(296, 41);
+            this.chkKeepRatio.Name = "chkKeepRatio";
+            this.chkKeepRatio.Size = new System.Drawing.Size(79, 17);
+            this.chkKeepRatio.TabIndex = 4;
+            this.chkKeepRatio.Text = "Keep Ratio";
+            this.chkKeepRatio.UseVisualStyleBackColor = true;
+            this.chkKeepRatio.CheckedChanged += new System.EventHandler(this.chkKeepRatio_CheckedChanged);
+            // 
+            // chkKeepTopOn
+            // 
+            this.chkKeepTopOn.AutoSize = true;
+            this.chkKeepTopOn.Checked = true;
+            this.chkKeepTopOn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkKeepTopOn.Location = new System.Drawing.Point(217, 41);
+            this.chkKeepTopOn.Name = "chkKeepTopOn";
+            this.chkKeepTopOn.Size = new System.Drawing.Size(73, 17);
+            this.chkKeepTopOn.TabIndex = 4;
+            this.chkKeepTopOn.Text = "Keep Top";
+            this.chkKeepTopOn.UseVisualStyleBackColor = true;
+            this.chkKeepTopOn.CheckedChanged += new System.EventHandler(this.chkKeepTopOn_CheckedChanged);
+            // 
+            // btnGO
+            // 
+            this.btnGO.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGO.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGO.Location = new System.Drawing.Point(384, 6);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(44, 29);
+            this.btnGO.TabIndex = 3;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblURL.Location = new System.Drawing.Point(9, 12);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(36, 17);
+            this.lblURL.TabIndex = 2;
+            this.lblURL.Text = "URL";
+            this.lblURL.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lblURL_MouseClick);
+            // 
+            // timHider
+            // 
+            this.timHider.Interval = 1000;
+            this.timHider.Tick += new System.EventHandler(this.timHider_Tick);
+            // 
+            // wbbYouTube
+            // 
+            this.wbbYouTube.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbbYouTube.Location = new System.Drawing.Point(0, 60);
+            this.wbbYouTube.Margin = new System.Windows.Forms.Padding(0);
+            this.wbbYouTube.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbbYouTube.Name = "wbbYouTube";
+            this.wbbYouTube.ScrollBarsEnabled = false;
+            this.wbbYouTube.Size = new System.Drawing.Size(495, 211);
+            this.wbbYouTube.TabIndex = 0;
+            this.wbbYouTube.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbbYouTube_DocumentCompleted);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 271);
+            this.ControlBox = false;
+            this.Controls.Add(this.wbbYouTube);
+            this.Controls.Add(this.pnlTop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YouWatch";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResizeBegin += new System.EventHandler(this.frmMain_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.frmMain_ResizeEnd);
+            this.MouseEnter += new System.EventHandler(this.frmMain_MouseEnter);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.CheckBox chkKeepTopOn;
+        private System.Windows.Forms.Timer timHider;
+        private System.Windows.Forms.WebBrowser wbbYouTube;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMaximize;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.CheckBox chkShowBorder;
+        private System.Windows.Forms.CheckBox chkKeepRatio;
+        private System.Windows.Forms.CheckBox chkShowInTaskbar;
+        private System.Windows.Forms.CheckBox chkMoveByMouse;
+    }
+}
+
---
+++ YouWatch/frmMain.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Windows.Forms;
+
+namespace YouWatch
+{
+    public partial class frmMain : Form
+    {
+        #region Shortcut
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int ACTIVATOR_HOTKEY_ID = 1;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == ACTIVATOR_HOTKEY_ID)
+            {
+                this.Focus();
+                this.ActiveControl = wbbYouTube;
+            }
+
+            base.WndProc(ref m);
+        }
+        #endregion
+
+        Size BeforeSize;
+
+        double Ratio = 0;
+
+        int timCounter = 0;
+
+        bool KeepRatio = false;
+        bool IsMoving = false;
+        bool ShowBorder = true;
+        bool IsResized = false;
+
+        string YoutubeURLPattern = "https://www.youtube.com/embed/{0}?";
+        string EmbedCodePattern = "<html><head><meta http-equiv='X-UA-Compatible' content='IE=edge'><style>*{margin:0px;padding:0px;}</style></head><iframe style='position:absolute; left:0; top:0; width:100%; height:100%' src='@URL@&autoplay=1&showinfo=1&controls=1&autohide=1' frameborder='0' allowfullscreen></iframe></html>";
+
+        private void ShowControls()
+        {
+            pnlTop.Visible = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+
+            if (!chkShowBorder.Checked)
+            {
+                if (this.Top < 0)
+                {
+                    this.Top = 0;
+                }
+
+                if (this.Left + this.Width > SystemInformation.VirtualScreen.Width)
+                {
+                    this.Left = SystemInformation.VirtualScreen.Width - this.Width;
+                }
+            }
+
+            this.Refresh();
+        }
+        private void HideControls()
+        {
+            pnlTop.Visible = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
+            timCounter = 0;
+            timHider.Enabled = false;
+
+            this.Refresh();
+        }
+
+        private string GenerateEmbedCode(string pURL)
+        {
+            var uri = new Uri(pURL);
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+
+            #region Video ID
+            var videoID = string.Empty;
+
+            if (query.AllKeys.Contains("v"))
+            {
+                videoID = query["v"];
+            }
+            else
+            {
+                videoID = uri.Segments.Last();
+            }
+            #endregion
+
+            string lastURL = string.Format(YoutubeURLPattern, videoID);
+
+            #region Time
+            string time = string.Empty;
+
+            if (query.AllKeys.Contains("t"))
+            {
+                time = query["t"];
+            }
+            #endregion
+
+            if (!string.IsNullOrEmpty(time))
+            {
+                lastURL = lastURL + "&start=" + time;
+            }
+
+            #region List
+            string list = string.Empty;
+
+            if (query.AllKeys.Contains("list"))
+            {
+                list = query["list"];
+            }
+            #endregion
+
+            if (!string.IsNullOrEmpty(list))
+            {
+                lastURL = lastURL + "&list=" + list;
+            }
+
+            string embedCode = EmbedCodePattern.Replace("@URL@", lastURL);
+
+            return embedCode;
+        }
+
+        private bool IsUrlValidForYouTube(string pURL)
+        {
+            string pattern = @"^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$";
+
+            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            return reg.IsMatch(pURL);
+        }
+
+        private void ShowVideo(string pURL)
+        {
+            if (!string.IsNullOrEmpty(pURL))
+            {
+                if (IsUrlValidForYouTube(pURL))
+                {
+                    pURL = pURL.Replace("#", "&");
+
+                    string embedCode = GenerateEmbedCode(pURL);
+
+                    wbbYouTube.DocumentText = "<html>" + embedCode + "</html>";
+                    wbbYouTube.ScriptErrorsSuppressed = true;
+                }
+                else
+                {
+                    MessageBox.Show("URL is not matched with YouTube!");
+                }
+            }
+        }
+
+        private string ReadURLFromClipboard()
+        {
+            string clip = Clipboard.GetText();
+
+            if (!string.IsNullOrEmpty(clip))
+            {
+                if (!IsUrlValidForYouTube(clip))
+                {
+                    clip = string.Empty;
+                }
+            }
+
+            return clip;
+        }
+
+        #region Mouse Move
+        public delegate void MouseMovedEvent();
+
+        public class GlobalMouseHandler : IMessageFilter
+        {
+            private const int WM_MOUSEMOVE = 0x0200;
+
+            public event MouseMovedEvent TheMouseMoved;
+
+            #region IMessageFilter Members
+
+            public bool PreFilterMessage(ref Message m)
+            {
+                if (m.Msg == WM_MOUSEMOVE)
+                {
+                    if (TheMouseMoved != null)
+                    {
+                        TheMouseMoved();
+                    }
+                }
+                // Always allow message to continue to the next filter control
+                return false;
+            }
+
+            #endregion
+        }
+        #endregion
+
+        private void MoveForm()
+        {
+            int diff = 20;
+
+            Point p = Cursor.Position;
+
+            if (Math.Abs(this.Left - p.X) < diff)
+            {
+                this.Left = this.Left + diff;
+            }
+            else if (Math.Abs((this.Left + this.Width) - p.X) < diff)
+            {
+                this.Left = this.Left - diff;
+            }
+
+            if (Math.Abs(this.Top - p.Y) < diff)
+            {
+                this.Top = this.Top + diff;
+            }
+            else if (Math.Abs((this.Top + this.Height) - p.Y) < diff)
+            {
+                this.Top = this.Top - diff;
+            }
+        }
+
+        public frmMain()
+        {
+            GlobalMouseHandler gmh = new GlobalMouseHandler();
+            gmh.TheMouseMoved += new MouseMovedEvent(frmMain_MouseMoved);
+            Application.AddMessageFilter(gmh);
+
+            InitializeComponent();
+
+            wbbYouTube.ScriptErrorsSuppressed = false;
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey(this.Handle, ACTIVATOR_HOTKEY_ID, 6, (int)Keys.Space);
+
+            txtURL.Text = ReadURLFromClipboard();
+
+            ShowVideo(txtURL.Text);
+        }
+        private void frmMain_ResizeBegin(object sender, EventArgs e)
+        {
+            this.BeforeSize = this.Size;
+
+            IsMoving = true;
+        }
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            this.IsResized = true;
+        }
+        private void frmMain_ResizeEnd(object sender, EventArgs e)
+        {
+            IsMoving = false;
+
+            if (this.IsResized && this.KeepRatio)
+            {
+                if (this.Height > this.BeforeSize.Height)
+                {
+                    this.Width = this.Width + Convert.ToInt32(this.Width * (1 - this.Ratio));
+                }
+                else if (this.Width > this.BeforeSize.Width)
+                {
+                    this.Height = this.Height + Convert.ToInt32(this.Height * (1 - this.Ratio));
+                }
+                else if (this.Height < this.BeforeSize.Height)
+                {
+                    this.Width = this.Width - Convert.ToInt32(this.Width * (1 - this.Ratio));
+                }
+                else if (this.Width < this.BeforeSize.Width)
+                {
+                    this.Height = this.Height - Convert.ToInt32(this.Height * (1 - this.Ratio));
+                }
+            }
+
+            this.IsResized = false;
+        }
+        private void frmMain_MouseMoved()
+        {
+            timCounter = 0;
+
+            if (!timHider.Enabled)
+            {
+                timHider.Enabled = true;
+            }
+        }
+        private void frmMain_MouseEnter(object sender, EventArgs e)
+        {
+            if (chkMoveByMouse.Checked && this.FormBorderStyle == FormBorderStyle.None)
+            {
+                MoveForm();
+            }
+        }
+
+        private void lblURL_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtURL.Text = ReadURLFromClipboard();
+
+            ShowVideo(txtURL.Text);
+        }
+
+        private void chkShowBorder_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkShowBorder.Checked)
+            {
+                ShowControls();
+
+                chkMoveByMouse.Checked = chkMoveByMouse.Enabled = false;
+
+                this.ShowBorder = true;
+            }
+            else
+            {
+                chkMoveByMouse.Enabled = true;
+
+                this.ShowBorder = false;
+            }
+        }
+        private void chkShowInTaskbar_CheckedChanged(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = chkShowInTaskbar.Checked;
+        }
+        private void chkKeepTopOn_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = chkKeepTopOn.Checked;
+        }
+        private void chkKeepRatio_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Ratio = Convert.ToDouble(this.Height) / Convert.ToDouble(this.Width);
+
+            this.KeepRatio = chkKeepRatio.Checked;
+        }
+
+        private void timHider_Tick(object sender, EventArgs e)
+        {
+            if (!this.IsMoving)
+            {
+                timCounter++;
+
+                var curPos = this.PointToClient(Cursor.Position);
+
+                int curX = curPos.X;
+                int curY = curPos.Y;
+
+                if (timCounter == 1)
+                {
+                    if ((curX > 0 && curX < this.Width) && (curY > 0 && curY < this.Height))
+                    {
+                        ShowControls();
+                    }
+                    else if (!this.ShowBorder)
+                    {
+                        HideControls();
+                    }
+                }
+                else if (timCounter > 5)
+                {
+                    if ((curX < 0 || curX > this.Width) || (curY < 0 || curY > this.Height))
+                    {
+                        if (!this.ShowBorder)
+                        {
+                            HideControls();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                ShowVideo(txtURL.Text);
+            }
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            ShowVideo(txtURL.Text);
+        }
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void btnMaximize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void wbbYouTube_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            wbbYouTube.Document.Body.MouseOver += new HtmlElementEventHandler(wbbYouTube_Body_MouseOver);
+        }
+        private void wbbYouTube_Body_MouseOver(object sender, HtmlElementEventArgs e)
+        {
+            if (chkMoveByMouse.Checked && this.FormBorderStyle == FormBorderStyle.None)
+            {
+                MoveForm();
+            }
+        }
+    }
+}
